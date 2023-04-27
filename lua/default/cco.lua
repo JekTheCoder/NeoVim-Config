636818496b48bfdd0be6c4ca92c0ca211f6dfd9b
Jeshua HInostroza <hinostrozajeshua@gmail.com>
2023-04-26 22:29:53 -0500

    feat: add support for cco (code-conjurer)

---
+++ lua/default/cco.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n", "<C-n>", function() 
+	local template_name = vim.fn.input("Template > ");
+	local name = vim.fn.input("Name > ");
+
+	local current_buffer_dir = vim.fn.expand("%:p:h");
+
+	vim.fn.system { "cco", "g", template_name, current_buffer_dir.."/"..name  };
+end)
