cd0b43cb25737bf52069a74e17fc3ca3d777ed3c
Jeshua HInostroza <hinostrozajeshua@gmail.com>
2023-05-03 21:53:09 -0500

    feat: install neo-tree

--- lua/default/packer.lua
+++ lua/default/packer.lua
@@ -56,4 +56,14 @@ return require('packer').startup(function(use)
 		'lewis6991/gitsigns.nvim',
 		-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
 	}
+
+	use {
+	  "nvim-neo-tree/neo-tree.nvim",
+	    branch = "v2.x",
+	    requires = { 
+	      "nvim-lua/plenary.nvim",
+		  "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	      "MunifTanjim/nui.nvim",
+		}
+	}
 end)
