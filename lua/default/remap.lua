2a19a9c42b7414d308c623ee891be5affc3cb35f
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-05-18 14:13:51 -0500

    config remaps for yank to system clipboard

--- lua/default/remap.lua
+++ lua/default/remap.lua
@@ -2,3 +2,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+
+-- Copy to system clipboard
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+yg_")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>yy", "\"+yy")
